b9cfc1ab2eaa872c609d09ee8702f125d1e9c852
rafaelcharris <rafaelcharris92@gmail.com>
2021-06-06 18:20:47 -0500

    agrgar el autor y la fecha del artículo

--- scrape_lasilla.py
+++ scrape_lasilla.py
@@ -14,8 +14,7 @@ from docx.shared import Inches
 from docx import Document
 import requests
 from bs4 import BeautifulSoup
-import os
-
+import re
 url = "https://lasillavacia.com/los-comunes-le-ofrecen-todo-petro-cambio-nada-concreto-81881"
 
 r = requests.get(url)
@@ -23,14 +22,15 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 # Get the artcle
 article = soup.find_all("div", "body-node-historia")
-# This is where the article is in the webpage
-#div class="body-node-historia
-
+autor_information = soup.find_all("div", "author author-top")
+autor = autor_information[0].find("div", "editor").find("a").text
+fecha = autor_information[0].find("div", "editor").find_all("span")[1].text
 # Create the word document that will store the info
 document = Document()
 
 document.add_heading(soup.title.text)
-
+document.add_paragraph('Autor: ' + autor, style = 'List Bullet')
+document.add_paragraph('Fecha: ' + fecha, style = 'List Bullet')
 p = document.add_paragraph(article[0].text)
 
 document.save('demo.docx')
