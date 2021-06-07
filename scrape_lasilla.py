da0076f556c4e9c2cc21543b6002e1c451ace826
rafaelcharris <rafaelcharris92@gmail.com>
2021-06-06 20:53:21 -0500

    agregar url como input y agregar scraping de la url para que seal nombre del artículo

--- scrape_lasilla.py
+++ scrape_lasilla.py
@@ -7,7 +7,7 @@ Parse out information such as the article title, updated date, and byline to ret
 
 The website I will scrape is la silla vacia
 
-Let's add another feture. It writes a word document with the paper
+Let's add another feature. It writes a word document with the paper
 '''
 
 from docx.shared import Inches
@@ -15,7 +15,13 @@ from docx import Document
 import requests
 from bs4 import BeautifulSoup
 import re
-url = "https://lasillavacia.com/los-comunes-le-ofrecen-todo-petro-cambio-nada-concreto-81881"
+
+#La url debería ser input del usuario
+#url = "https://lasillavacia.com/los-comunes-le-ofrecen-todo-petro-cambio-nada-concreto-81881"
+url = input('Escriba la url de La Silla: ')
+name_info = re.match("(https://lasillavacia.com/)(.*\D)", url)
+
+name = name_info.group(2).replace("-", " ")[0:20]
 
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
@@ -24,7 +30,9 @@ soup = BeautifulSoup(r.text, 'html.parser')
 article = soup.find_all("div", "body-node-historia")
 autor_information = soup.find_all("div", "author author-top")
 autor = autor_information[0].find("div", "editor").find("a").text
-fecha = autor_information[0].find("div", "editor").find_all("span")[1].text
+fecha_info = autor_information[0].find("div", "editor").find_all("span")[1].text
+
+fecha = re.match(r"\n(.)", fecha_info)
 # Create the word document that will store the info
 document = Document()
 
@@ -33,5 +41,5 @@ document.add_paragraph('Autor: ' + autor, style = 'List Bullet')
 document.add_paragraph('Fecha: ' + fecha, style = 'List Bullet')
 p = document.add_paragraph(article[0].text)
 
-document.save('demo.docx')
+document.save(name + '.docx')
 print("Documento Listo")
